29df0f09d031910c17681ae9df0b71ae325cdaa9
Nickdaco <nickdaco@gmail.com>
2020-06-04 14:02:27 -0400

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,76 @@
+from tkinter import *
+from main import MorseTranslate
+
+root = Tk()
+
+root.title('Morse Translator')
+
+# .... . .-.. .-.. ---  .-- --- .-. .-.. -.. 
+# Enter Label
+enterLabel = Label(root, text='Enter:',)
+enterLabel.grid(row=0, column=0)
+
+# Allow user to type
+enter = Entry(root, width=35)
+enter.grid(row=0, column=1, columnspan=3, padx=10, pady=20)
+
+outputLabel = Label(root)
+
+# Copy Button
+def copyClick():
+    morseText = MorseTranslate().morseTranslateText(enter.get())
+    r = Tk()
+    r.withdraw()
+    r.clipboard_clear()
+    r.clipboard_append(morseText)
+    print("Copied!")
+
+copyButton = Button(root, text='Copy to Clipboard', command=copyClick)
+
+
+# Button Press Logic
+def textClick():
+    # Clear old stuff written in outputLabel
+    global outputLabel
+    outputLabel.destroy()
+    # Get new stuff and display
+    morseText = MorseTranslate().morseTranslateText(enter.get())
+    outputLabel = Label(root, text=morseText)
+    outputLabel.grid(row=2, column=1, columnspan=5, padx=30, pady=20)
+    # Add copy button
+    copyButton.grid(row=2, column=0, padx=10)
+
+
+def bothClick():
+
+    # Clear old stuff written in outputLabel
+    global outputLabel
+    outputLabel.destroy()
+    # Get new stuff and display
+    morseText = MorseTranslate().morseTranslateText(enter.get())
+    outputLabel = Label(root, text=morseText)
+    outputLabel.grid(row=2, column=1, columnspan=5, padx=30, pady=20)
+    # Play sound
+    MorseTranslate().morseTranslateBoth(enter.get())
+    # Add copy button
+    copyButton.grid(row=2, column=0)
+
+
+def audioClick():
+    # Play sound
+    MorseTranslate().morseTranslateAudio(enter.get())
+
+
+# Buttons
+textButton = Button(root, text='Text', command=textClick)
+bothButton = Button(root, text='Both', command=bothClick)
+audioButton = Button(root, text='Audio', command=audioClick)
+copyButton = Button(root, text='Copy to Clipboard', command=copyClick)
+
+# Add Buttons to grid
+textButton.grid(row=1, column=1)
+bothButton.grid(row=1, column=2)
+audioButton.grid(row=1, column=3)
+
+
+root.mainloop()
---
+++ main.py
@@ -0,0 +1,82 @@
+import pygame as pg
+
+# Uses pygame mixer to play sound
+
+def playMorse(letter): # play the morse code
+    file = '/Users/Nicksmac/Desktop/PyCharm/MorseCodeTranslator/MorseCode/Morse-{}.ogg.mp3'.format(letter)
+    volume = 1
+
+    # initializes mixer
+    pg.mixer.init()
+    pg.mixer.music.set_volume(volume)
+
+    pg.mixer.music.load(file)
+    pg.mixer.music.play()
+
+    clock = pg.time.Clock()
+    while pg.mixer.music.get_busy():
+
+        # check if playback has finished
+        clock.tick(30)
+
+morseDict =\
+    {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+     'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+     'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.','O': '---', 'P': '.--.',
+     'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-',
+     'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..','1': '.----', '2': '..---',
+     '3': '...--', '4': '....-', '5': '.....', '6': '-....','7': '--...', '8': '---..',
+     '9': '----.', '0': '-----', ', ': '--..--', '.': '.-.-.-','?': '..--..', '/': '-..-.',
+     '-': '-....-', '(': '-.--.', ')': '-.--.-', " ": " "}
+
+
+# Class contains all different types of translations
+class MorseTranslate:
+
+    def morseTranslateBoth(self, input):
+
+        string = ""
+        for letter in input:
+            if letter == " ":
+                string += morseDict[letter.upper()]
+
+            else:
+                try:
+                    string += morseDict[letter.upper()] + ' '
+                    playMorse(letter)
+                except KeyError:
+                    pass
+
+        print(string)
+        return string
+
+
+    def morseTranslateAudio(self, input):
+
+        for letter in input:
+            if letter == " ":
+                pass
+
+            else:
+                try:
+                    playMorse(letter)
+                except KeyError:
+                    pass
+
+
+    def morseTranslateText(self, input):
+
+        string = ""
+        for letter in input:
+            if letter == " ":
+                string += morseDict[letter.upper()]
+            else:
+
+                try:
+                    string += morseDict[letter.upper()] + ' '
+                except KeyError:
+                    pass
+        print(string)
+        return string
+
+
